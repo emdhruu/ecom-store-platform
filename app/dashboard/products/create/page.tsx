81a1b50f1143facc615a74e002cebcc4ebb120e7
Dhrutii <thakkardhruti22@gmail.com>
2025-02-27 15:28:51 +0530

    feat(ui) : create product page

---
+++ app/dashboard/products/create/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+import { UploadButton, UploadDropzone } from "@/app/lib/uploadthing";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function ProductCreateRoute() {
+  return (
+    <form>
+      <div className="flex items-center gap-3">
+        <Button size="icon" variant="outline" asChild>
+          <Link href="/dashboard/products">
+            <ChevronLeft className="w-4 h-4" />
+          </Link>
+        </Button>
+        <h1 className="text-xl font-semibold tracking-tight">New Product</h1>
+      </div>
+      <Card className="mt-5">
+        <CardHeader>
+          <CardTitle>Product Detail</CardTitle>
+          <CardDescription>
+            In this form your can create your product
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3">
+              <Label>Product Name</Label>
+              <Input type="text" placeholder="Product Name" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Description</Label>
+              <Textarea placeholder="Write product description here" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Price</Label>
+              <Input type="number" placeholder="$30.00" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Featured Product</Label>
+              <Switch />
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Status</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select Status here" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="draft">Draft</SelectItem>
+                  <SelectItem value="archived">Archived</SelectItem>
+                  <SelectItem value="published">Publised</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Label>Images</Label>
+              <UploadDropzone endpoint="imageUploader" />
+              {/* <UploadButton endpoint="imageUploader" /> */}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
+  );
+}
